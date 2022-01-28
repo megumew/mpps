cfde1dcb666c402324ddfaeaf8558e22704b4519
megumew <megumeww@gmail.com>
2022-01-27 20:26:53 -0500

    Added basic structs to track members and expenses

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::LinkedList;
+
+#[derive(Debug)]
+struct Expense {
+    title: String,
+    amount: f32,
+    paid_by: Member,
+    owe: LinkedList<Member>,
+}
+
+#[derive(Debug)]
+struct Member {
+    name: String,
+    expenses: LinkedList<Expense>,
+}
+
+impl Member {
+    fn new(name: String) -> Self {
+        Member{
+            name,
+            expenses: LinkedList::<Expense>::new()
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let megu = Member::new(String::from("Megu"));
+    println!("{:?}", megu);
 }
