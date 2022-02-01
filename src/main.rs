166b31062d28499dfea5c49bec8964b962cb90ff
megumew <megumeww@gmail.com>
2022-02-01 10:18:45 -0500

    Attempted to add expenses that track the person who paid but needs lifetimes to be implemented

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,19 @@ use std::collections::LinkedList;
 struct Expense {
     title: String,
     amount: f32,
-    paid_by: Member,
+    paid_by: &Member,
     owe: LinkedList<Member>,
+}
+
+impl Expense {
+    fn new(title:String, amount: f32, paid_by: &Member, owe:LinkedList<Member> ) -> Self {
+        Expense{
+            title,
+            amount,
+            paid_by,
+            owe,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -25,5 +36,10 @@ impl Member {
 
 fn main() {
     let megu = Member::new(String::from("Megu"));
-    println!("{:?}", megu);
+    let eli = Member::new(String::from("Eli"));
+
+    let food = Expense::new(String::from("Drive-Thru"), 16.38, &eli, LinkedList::new());
+
+    println!("{} has {} expenses.", megu.name, megu.expenses.len());
+    println!("{} has {} expenses.", eli.name, eli.expenses.len());
 }
