d3594135d3a9444c4c3d783dc6684f644a8560fb
megumew <megumeww@gmail.com>
2022-02-02 23:59:48 -0500

    Refactored to remove the lifetime erros.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
-use std::collections::LinkedList;
+use std::collections::HashMap;
+
+type MemberId = u32;
 
 #[derive(Debug)]
-struct Expense {
-    title: String,
-    amount: f32,
-    paid_by: &Member,
-    owe: LinkedList<Member>,
+struct Ledger {
+    transactions: Vec<Transaction>,
+    members: HashMap<MemberId, Member>,
+    balances: HashMap<MemberId, f32>,
+    ids: u32,
 }
 
-impl Expense {
-    fn new(title:String, amount: f32, paid_by: &Member, owe:LinkedList<Member> ) -> Self {
-        Expense{
+impl Ledger {
+    fn new() -> Self {
+        Ledger {
+            transactions: vec![],
+            members: HashMap::new(),
+            balances: HashMap::new(),
+            ids: 0,
+        }
+    }
+
+    fn add_member(&mut self, name: &str) {
+        self.ids += 1;
+        self.members
+            .insert(self.ids, Member::new(name.to_string(), self.ids));
+    }
+}
+
+#[derive(Debug)]
+struct Transaction {
+    title: String,
+    amount: i64,
+    from: MemberId,
+    to: MemberId,
+}
+
+impl Transaction {
+    fn new(title: String, amount: i64, from: u32, to: u32) -> Self {
+        Transaction{
             title,
             amount,
-            paid_by,
-            owe,
+            from,
+            to,
         }
     }
 }
@@ -22,24 +49,29 @@ impl Expense {
 #[derive(Debug)]
 struct Member {
     name: String,
-    expenses: LinkedList<Expense>,
+    id: MemberId,
+    transactions: Vec<Transaction>,
 }
 
 impl Member {
-    fn new(name: String) -> Self {
-        Member{
+    fn new(name: String, id: u32) -> Self {
+        Member {
             name,
-            expenses: LinkedList::<Expense>::new()
+            transactions: vec![],
+            id,
         }
     }
 }
 
 fn main() {
-    let megu = Member::new(String::from("Megu"));
-    let eli = Member::new(String::from("Eli"));
+    let mut ledger = Ledger::new();
+    ledger.add_member("Megu");
+    ledger.add_member("Eli");
 
-    let food = Expense::new(String::from("Drive-Thru"), 16.38, &eli, LinkedList::new());
+    let test = Transaction::new(String::from("Drive-Thru"), 1638, 0, 1);
 
-    println!("{} has {} expenses.", megu.name, megu.expenses.len());
-    println!("{} has {} expenses.", eli.name, eli.expenses.len());
+    ledger.transactions.push(test);
+
+    println!("There are {} members", ledger.ids);
+    println!("{:?}", ledger);
 }
